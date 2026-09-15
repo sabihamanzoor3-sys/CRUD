2a63a39ff0e146a15fa25c6adb41204fae1ed79b
Sabiha Manzoor <sabihamanzoor3@gmail.com>
2026-09-15 11:05:51 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,68 +4,48 @@ const cors = require("cors");
 const UserModel = require("./models/Users");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-mongoose
-.connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log("MongoDB Error:", err));
+mongoose.connect("mongodb://127.0.0.1:27017/CRUD");
 
-app.get("/", async (req, res) => {
-try {
-const users = await UserModel.find({});
-res.json(users);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+app.get("/", (req, res) => {
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
 });
 
-app.get("/getUser/:id", async (req, res) => {
-try {
-const user = await UserModel.findById(req.params.id);
-res.json(user);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+app.get("/getUser/:id", (req, res) => {
+    const id = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
 });
 
-app.put("/updateUser/:id", async (req, res) => {
-try {
-const user = await UserModel.findByIdAndUpdate(
-req.params.id,
-{
-name: req.body.name,
-email: req.body.email,
-age: req.body.age
-},
-{ new: true }
-);
-
-    res.json(user);
-} catch (err) {
-    res.status(500).json({ error: err.message });
-}
-
+app.put("/updateUser/:id", (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id:id}, {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    })
+     .then(users => res.json(users))
+    .catch(err => res.json(err))
 });
 
-app.delete("/deleteUser/:id", async (req, res) => {
-try {
-const user = await UserModel.findByIdAndDelete(req.params.id);
-res.json(user);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+app.delete("/deleteUser/:id", (req, res)=> {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+    .then(res => res.json(res))
+    .catch(err => res.json(err))
 });
 
-app.post("/createUser", async (req, res) => {
-try {
-const user = await UserModel.create(req.body);
-res.json(user);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
 });
 
-module.exports = app;
+app.listen(3001, () => {
+    console.log("Server is Running");
+})
